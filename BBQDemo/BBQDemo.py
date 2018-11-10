04563fcfdadfc20803b93882a077dd3dd7609938
hexy717@163.com <hexy717@163.com>
2018-11-10 16:20:25 -0600

    add upload

--- BBQDemo/BBQDemo.py
+++ BBQDemo/BBQDemo.py
@@ -1,12 +1,24 @@
-from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
+UPLOAD_FOLDER = './uploads/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
+      return 'file uploaded successfully'
 
 if __name__ == '__main__':
 	app.run(debug=True)
