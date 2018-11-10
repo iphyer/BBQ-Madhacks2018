95ddb13d291a45d4d6f56eebeafb15d8d4155703
hexy717@163.com <hexy717@163.com>
2018-11-10 17:10:10 -0600

    modified upload

--- BBQDemo/BBQDemo.py
+++ BBQDemo/BBQDemo.py
@@ -8,17 +8,28 @@ bootstrap = Bootstrap(app)
 
 UPLOAD_FOLDER = './uploads/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+ALLOWED_EXTENSIONS_IMG = set(['png', 'jpg', 'jpeg', 'gif'])
+ALLOWED_EXTENSIONS_TEXT = set(['csv', 'txt'])
+
+def allowed_file(filename, mode):
+    if mode == 'img':
+        return '.' in filename and \
+               filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS_IMG
+    elif mode == 'text':
+        return '.' in filename and \
+               filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS_TEXT
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
-      return 'file uploaded successfully'
+    if request.method == 'POST':
+        files = request.files.getlist("file")
+        for f in files:
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
+        return 'file uploaded successfully'
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
