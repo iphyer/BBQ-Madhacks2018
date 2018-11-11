6950d231fcaa9079b39796e20ee0da1e4495dae4
hexy717@163.com <hexy717@163.com>
2018-11-11 02:43:45 -0600

    add zoomin

--- BBQDemo/util/plotHelper.py
+++ BBQDemo/util/plotHelper.py
@@ -6,9 +6,8 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
 
-def plot(array, dirPath):
+def plotloc(loc_list, dirPath):
     matplotlib.use('Agg')
-
 
     # truth = np.loadtxt(filename, delimiter=',' )
     fig = plt.figure()
@@ -25,10 +24,9 @@ def plot(array, dirPath):
         1 : "blue",
         2 : "yellow"
     }
-    for element in array:
-        print(element)
+
     # Adding blue rectangle from predict
-    for element in array:
+    for element in loc_list:
         rect = element[0]
         recList.append(
             patches.Rectangle(
@@ -40,23 +38,59 @@ def plot(array, dirPath):
                 linewidth=2
             )
         )
-
-    # # Adding red rectangle for truth
-    # for rect in truth:
-    #     recList.append(
-    #         patches.Rectangle(
-    #             (rect[0], rect[3]),
-    #              np.abs(rect[2] - rect[0]),
-    #              np.abs(rect[3] - rect[1]),
-    #              fill=False,
-    #              edgecolor = "red"
-    #         )
-    #     )
-
     # plot the graph
     for p in recList:
         ax.add_patch(p)
 
     plt.plot()
     # plt.show()
-    fig.savefig(os.path.join(dirPath, "out.png"), dpi=190, bbox_inches='tight', pad_inches=0)
+    fig.savefig(os.path.join(dirPath, "loc.png"), dpi=190, bbox_inches='tight', pad_inches=0)
+
+
+
+def plotcls(cls_list, dirPath):
+    matplotlib.use('Agg')
+
+
+    # truth = np.loadtxt(filename, delimiter=',' )
+    fig = plt.figure()
+    ax = fig.add_subplot(111, aspect='equal')
+    im = np.array(Image.open(os.path.join(dirPath, "image")), dtype=np.uint8)
+    ax.set_axis_off()
+    print("here")
+
+    ax.imshow(im.astype(np.uint8), cmap='gray')
+    recList = list()
+
+    # Adding blue rectangle from predict
+    for element in cls_list:
+        pred = element[0]
+        gt = element[2]
+        recList.append(
+            patches.Rectangle(
+                (pred[1], pred[2]),
+                np.abs(pred[3] - pred[1]),
+                np.abs(pred[4] - pred[2]),
+                fill=False,
+                edgecolor = 'red',
+                linewidth=1
+            )
+        )
+
+        recList.append(
+            patches.Rectangle(
+                (gt[1], gt[2]),
+                np.abs(gt[3] - gt[1]),
+                np.abs(gt[4] - gt[2]),
+                fill=False,
+                edgecolor = 'blue',
+                linewidth=1
+            )
+        )
+    # plot the graph
+    for p in recList:
+        ax.add_patch(p)
+
+    plt.plot()
+    # plt.show()
+    fig.savefig(os.path.join(dirPath, "cls.png"), dpi=190, bbox_inches='tight', pad_inches=0)
